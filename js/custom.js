51c320581271f30b0416c9f725e9cd75441b8cba
Patryk Mazurkiewicz <patrykmazurkiewicz@MacBook-Pro-Patryk.local>
2017-02-09 10:21:00 +0100

    comp

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,12 @@
 function getPromise(start, end){
     var root = 'https://jsonplaceholder.typicode.com',
-        option = '/photos?_start=' + start + '&_end=' + end;
+        params = '/photos?_start=' + start + '&_end=' + end;
 
     if(window.Promise){
         return new Promise(function(resolve, reject){
             var req = new XMLHttpRequest();
 
-            req.open('GET', root + option);
+            req.open('GET', root + params);
 
             req.onload = function(){
                 if(req.status === 200){
@@ -37,7 +37,6 @@ function getPromise(start, end){
 var promise = getPromise('00', '20');
 promise.then(function(data){
     console.log('Got data! Promise fulfilled');
-    console.log(JSON.parse(data));
     render(JSON.parse(data));
 }, function(err){
     console.log('Promise rejected.');
@@ -45,11 +44,32 @@ promise.then(function(data){
 });
 
 function render(data){
-    var elems = data.map(function(item){
-        return React.createElement('li', {key: item.id},
-            React.createElement('h2', {}, item.title),
-            React.createElement('p', {dangerouslySetInnerHTML: {__html: '<img src="' + item.thumbnailUrl + '"/>'}})
-        )
+    console.log(data);
+    var EntryItem = React.createClass({
+        propTypes: {
+            index: React.PropTypes.number.isRequired,
+            properties: React.PropTypes.object.isRequired
+        },
+
+        render: function() {
+            return (
+                React.createElement('li', {'data-index': this.props.index},
+                    React.createElement('h2', {}, this.props.properties.title),
+                    React.createElement('img', {src: this.props.properties.thumbnailUrl})
+                )
+            )
+        }
+    });
+    var elems = data.map(function(dataItem, index) {
+        var properties = {
+            title: dataItem.title,
+            thumbnailUrl: dataItem.thumbnailUrl
+        };
+        return React.createElement(EntryItem, {
+            key: dataItem.id,
+            index: index,
+            properties: properties
+        });
     });
     var listOfElems = React.createElement('ul', {}, elems);
     ReactDOM.render(listOfElems, document.getElementById('app'));
